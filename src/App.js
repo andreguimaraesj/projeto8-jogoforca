d59bd0f64e98a08e4e423e5377cc88f2122511c8
andreguimaraesj <dev.detto@gmail.com>
2023-05-15 11:45:31 -0300

    componentizaçao inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Jogo from "./Jogo";
+import Letras from "./Letras";
 
 function App() {
+  const alfabeto = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const [palavraSecreta, setPalavraSecreta] = useState("");
+  const [palavraUsuario, setPalavraUsuario] = useState([]);
+  const [palpites, setPalpites] = useState([]);
+  const [teclas, setTeclas] = useState(
+    alfabeto.map((letra) => (
+      <button key={letra} disabled data-test="letter">
+        {letra.toUpperCase()}
+      </button>
+    ))
+  );
+  const [erros, setErros] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+    <div className="container">
+      <Jogo
+        erros={erros}
+        setErros={setErros}
+        palavraUsuario={palavraUsuario}
+        setPalavraUsuario={setPalavraUsuario}
+        palavraSecreta={palavraSecreta}
+        setPalavraSecreta={setPalavraSecreta}
+        alfabeto={alfabeto}
+        teclas={teclas}
+        setTeclas={setTeclas}
+        palpites={palpites}
+        setPalpites={setPalpites}
+      />
+      <Letras teclas={teclas} />
     </div>
   );
 }
--- src/palavras.js
+++ src/palavras.js
@@ -1,1 +1,26 @@
- 
+const palavras = [
+    "abacaxi", "abacate", "alecrim", "abelha", "amor", "analfabeto", "antes", "aberto", "abrir",
+    "bonde", "banana", "bacalhau", "batata", "bailarina", "bexiga", "bisturi", "barreira", "banco",
+    "cantor", "caixa", "cuidado", "cachorro", "carinho", "caneta", "corrigir", "cabra",
+    "domingo", "dados", "dia", "detonar", "diafragma", "danone", "dente", "diamante",
+    "esmeralda", "elefante", "encontro", "encanto", "escola", "economia", "esfinge", "esfera",
+    "festa", "formiga", "felicidade", "flauta", "fotografia", "fevereiro", "figo", "fiapo",
+    "gelo", "goiaba", "golfinho", "gentileza", "goleiro", "golfe", "grito", "girar",
+    "helicoptero", "hidrante", "haste", "harmonia", "hiena", "hectare",
+    "inefavel", "idoso", "importante", "idioma", "identidade", "idade", "idiota", "igreja",
+    "jantar", "jararaca", "jaula", "jaleco", "jurubeba", "jumento", "janela", "jabuti",
+    "luz", "laranja", "lindo", "lua", "lacraia", "latim", "lacrar",
+    "macarrao", "matar", "manga", "mocassin", "mingau", "morango", "mico",
+    "narina", "narcisista", "ninja", "nicaragua", "navio", "nada", "nectarina", "namorado", "natal",
+    "orangotango", "original", "ornamento", "ouro", "olhar", "ortodoxo", "ostra", "ovo",
+    "princesa", "pano", "pizza", "patroa", "prova", "pitu", "polvo",
+    "quinta", "queijo", "quantia", "quaresma", "quarentena", "quebrar", "quadrilha", "quartzo", "quitanda",
+    "rato", "risada", "racional", "radioatividade", "raiz", "raiva", "rainha", "remar", "rachadura", "rio",
+    "sacola", "salada", "sabre", "siri", "sacerdote", "sucarose", "sabedoria", "salamandra",
+    "tamanho", "tatu", "trem", "torrada", "tabacaria", "taberneiro",
+    "universidade", "unanimidade", "ubuntu", "uivar", "urso", "unir", "uva", "vaca", "vidro", "vagem",
+    "valentia", "valor", "valeta", "vela", "vadiagem",
+    "xerife", "xadrez", "xarope", "xereta",
+]
+
+export default palavras
